f3c2fb814e6bac72bbddeff228e0f1c75c568b78
Roger Robertson Jr <rjrjr@pm.me>
2020-06-03 16:21:29 -0700

    models, controllers, serializers, beginning auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :user_conversations
+    has_many :conversations, through: :user_conversations
+    has_many :messages
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 end
