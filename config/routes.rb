35a7457af4ca2adf4ef1f8a1288815e803b2fbe5
Roger Robertson Jr <rjrjr@pm.me>
2020-06-03 16:43:22 -0700

    beginning auth with bcrypt and jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:create]
+  post '/login', to: 'auth#create'
+  get '/home', to: 'users#show'
+  
   resources :messages
   resources :conversations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
