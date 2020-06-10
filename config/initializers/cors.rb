9f45d89c816a9977f4dfb7653e429aa1f1fc4432
Roger Robertson Jr <rjrjr@pm.me>
2020-06-10 16:21:55 -0700

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://objective-lamport-f498dc.netlify.app'
+    origins 'https://emissary.netlify.app'
 
     resource '*',
       headers: :any,
